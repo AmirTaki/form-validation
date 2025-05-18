f2d1b0721f99cb355e04e69ca6cb5c30fc3ae036
amir <amirtaki623@gmail.com>
2025-05-18 21:12:40 +0330

    js : validation email

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const submitBtn = document.getElementById("submitBtn")
 const nameErorr = document.getElementById('nameError')
+const emailError = document.getElementById('emailError')
 
 const validationName  =() =>{
     let name = document.getElementById('name').value
@@ -22,9 +23,29 @@ const validationName  =() =>{
 }
 
 
+const validationEmail = () => {
+    let email  = document.getElementById('email').value
 
+       if ( email.length  == 0 ){
+        emailError.innerHTML = "Email is required";
+        emailError.previousElementSibling.classList.add('bi-x')
+        return false
+    }
+      if (!email.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+        emailError.innerHTML = 'Write full Email'
+        emailError.previousElementSibling.classList.add('bi-x')
+        return false
+
+        
+    }
+    emailError.innerHTML = ''
+    emailError.previousElementSibling.classList.add('bi-check-lg')    
+    return true
+
+}
 submitBtn.addEventListener('click', (e)=>{
     e.preventDefault();
-    validationName() ? alert ('From Submitted Successfully') : ''
+    validationName() &&  validationEmail() ? alert ('From Submitted Successfully') : ''
+
 
 })
