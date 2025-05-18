39b211a1271d80f00b57be787b5a8508e388f7ad
amir <amirtaki623@gmail.com>
2025-05-18 21:05:03 +0330

    js : valid name =

--- src/main.js
+++ src/main.js
@@ -5,20 +5,23 @@ const validationName  =() =>{
     let name = document.getElementById('name').value
 
     if ( name.length  == 0 ){
-        nameErorr.innerHTML = "Name is required"
+        nameErorr.innerHTML = "Name is required";
+        nameErorr.previousElementSibling.classList.add('bi-x')
         return false
     }
 
     if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
         nameErorr.innerHTML = 'Write full Name'
+        nameErorr.previousElementSibling.classList.add('bi-x')
         return false
     }
-        
+    nameErorr.innerHTML = ''
+    nameErorr.previousElementSibling.classList.add('bi-check-lg')    
     return true
 
-
-    
 }
+
+
 
 submitBtn.addEventListener('click', (e)=>{
     e.preventDefault();
